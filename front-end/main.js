dc7633996f1c97a54c857f891937fa19b10b1e07
Johan-web-devloper <johan.flore.13@gmail.com>
2023-08-08 07:31:55 -0500

    ahi vamos

---
+++ front-end/main.js
@@ -0,0 +1,17 @@
+let onebtn = document.getElementsByClassName("uno");
+let twobtn = document.getElementsByClassName("dos");
+let threebtn = document.getElementsByClassName("tres");
+let imgchange = document.getElementById("imagechange");
+
+
+onebtn.onclick = function(){
+    imagechange.src = "./imgs/6115623-removebg-preview.png";
+}
+
+twobtn.onclick = function(){
+    imagechange.src = "./imgs/6115613-removebg-preview.png";
+}
+
+threebtn.onclick = function(){
+    imagechange.src = "./imgs/6115690-removebg-preview.png";
+}
